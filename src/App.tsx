904bc2d5d774ce1f2530d37385b1391deebea9a4
gakkunn <yoshizumi.free01@gmail.com>
2023-09-28 05:31:02 +0900

    [add]Todoの追加機能を設けた

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { MouseEvent, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    id: number;
+    inputValue: string;
+    checked: boolean;
+    completedOn?: string;
+  };
+
+  //入力値をinputValueに格納
+  const handleNewTodoTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  //inputValueを用いてtodosにオブジェクトを配列として格納
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const newTodo: Todo = {
+      id: todos.length,
+      inputValue: inputValue,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue('');
+  };
 
   return (
     <div className="App">
@@ -12,9 +35,13 @@ function App() {
       <div className="todo-wrapper">
         <div className="todo-input">
           <div className="todo-input-item">
-            <label>Title:</label>
-            <input />
-            <button>Add</button>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <label>Title:</label>
+              <input type="text" onChange={(e) => handleNewTodoTitle(e)} />
+              <button type="button" onClick={(e) => handleSubmit(e)}>
+                Add
+              </button>
+            </form>
           </div>
         </div>
         <div className="change-buttons-wrapper">
@@ -22,28 +49,18 @@ function App() {
           <button className="change-button-active">Completed</button>
         </div>
         <div className="todo-list-wrapper">
-          <div className="todo-list-item">
-            <div className="todo-list-item-title">
-              <h3>
-                スタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてる
-              </h3>
-              <strong className="todo-list-item-time">9月28日</strong>
+          {todos.map((todo) => (
+            <div key={todo.id} className="todo-list-item">
+              <div className="todo-list-item-title">
+                <h3>{todo.inputValue}</h3>
+                <strong className="todo-list-item-time">9月28日</strong>
+              </div>
+              <div className="todo-list-item-button">
+                <button className="todo-list-item-complete">完了</button>
+                <button className="todo-list-item-delete">削除</button>
+              </div>
             </div>
-            <div className="todo-list-item-button">
-              <button className="todo-list-item-delete">削除</button>
-              <button className="todo-list-item-complete">完了</button>
-            </div>
-          </div>
-          <div className="todo-list-item">
-            <div className="todo-list-item-title">
-              <h3>スタイルをあてる</h3>
-              <strong className="todo-list-item-time">9月28日</strong>
-            </div>
-            <div className="todo-list-item-button">
-              <button className="todo-list-item-delete">削除</button>
-              <button className="todo-list-item-complete">完了</button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
