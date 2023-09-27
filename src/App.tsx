01118e4d0f89d36cedd31c87dacef9a2af62eda8
gakkunn <yoshizumi.free01@gmail.com>
2023-09-28 03:21:49 +0900

    [add]スタイルを調整
    App.tsxにjsxを記入後、App.cssにスタイルを当てた

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1 className="todo-title">Todo List</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title:</label>
+            <input />
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="change-buttons-wrapper">
+          <button className="change-button">To Do</button>
+          <button className="change-button-active">Completed</button>
+        </div>
+        <div className="todo-list-wrapper">
+          <div className="todo-list-item">
+            <div className="todo-list-item-title">
+              <h3>
+                スタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてるスタイルをあてる
+              </h3>
+              <strong className="todo-list-item-time">9月28日</strong>
+            </div>
+            <div className="todo-list-item-button">
+              <button className="todo-list-item-delete">削除</button>
+              <button className="todo-list-item-complete">完了</button>
+            </div>
+          </div>
+          <div className="todo-list-item">
+            <div className="todo-list-item-title">
+              <h3>スタイルをあてる</h3>
+              <strong className="todo-list-item-time">9月28日</strong>
+            </div>
+            <div className="todo-list-item-button">
+              <button className="todo-list-item-delete">削除</button>
+              <button className="todo-list-item-complete">完了</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
