38ca39ed488b1f6d493e870c92513db65039f452
gakkunn <yoshizumi.free01@gmail.com>
2023-09-28 06:47:56 +0900

    [add]Todoの追加機能を設けた

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     setInputValue(e.target.value);
   };
 
-  //[追加機能] inputValueを用いてtodosにオブジェクトを配列として格納
+  //[追加機能] 追加ボタンを押した時に、オブジェクトを作成
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     const newTodo: Todo = {
       id: todos.length,
@@ -31,7 +31,19 @@ function App() {
     setInputValue('');
   };
 
-  // [完了機能]
+  // [完了機能] 完了ボタンを押した時に、そのTodoのcheckedをtrueにする
+  const handleComplete = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              checked: true,
+            }
+          : todo
+      )
+    );
+  };
 
   // [削除機能]
   const handleDelete = (id: number) => {
@@ -48,7 +60,7 @@ function App() {
               <label>Title:</label>
               <input type="text" onChange={(e) => handleNewTodoTitle(e)} />
               <button type="button" onClick={(e) => handleSubmit(e)}>
-                Add
+                追加
               </button>
             </form>
           </div>
@@ -58,33 +70,44 @@ function App() {
             className={`change-button${!isCompletedScreen ? '-active' : ''}`}
             onClick={() => setIsCompletedScreen(false)}
           >
-            To Do
+            未完了
           </button>
           <button
             className={`change-button${isCompletedScreen ? '-active' : ''}`}
             onClick={() => setIsCompletedScreen(true)}
           >
-            Completed
+            完了
           </button>
         </div>
         <div className="todo-list-wrapper">
-          {todos.map((todo) => (
-            <div key={todo.id} className="todo-list-item">
-              <div className="todo-list-item-title">
-                <h3>{todo.inputValue}</h3>
-                <strong className="todo-list-item-time">{todo.datetime}</strong>
+          {todos
+            .filter((todo) => isCompletedScreen === todo.checked)
+            .map((todo) => (
+              <div key={todo.id} className="todo-list-item">
+                <div className="todo-list-item-title">
+                  <h3>{todo.inputValue}</h3>
+                  <strong className="todo-list-item-time">
+                    {todo.datetime}
+                  </strong>
+                </div>
+                <div className="todo-list-item-button">
+                  {!todo.checked && (
+                    <button
+                      className="todo-list-item-complete"
+                      onClick={() => handleComplete(todo.id)}
+                    >
+                      完了
+                    </button>
+                  )}
+                  <button
+                    className="todo-list-item-delete"
+                    onClick={() => handleDelete(todo.id)}
+                  >
+                    削除
+                  </button>
+                </div>
               </div>
-              <div className="todo-list-item-button">
-                <button className="todo-list-item-complete">完了</button>
-                <button
-                  className="todo-list-item-delete"
-                  onClick={() => handleDelete(todo.id)}
-                >
-                  削除
-                </button>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
